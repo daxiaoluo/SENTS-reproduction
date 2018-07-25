4f8349321ba2b812a90f1257163d377a71fe9c1d
Hamza Ezzarouali El Boudri <hapaa@hotmail.fr>
2018-07-25 16:46:11 +0900

    Organization changes

--- scripts/text_to_paragraphs.py
+++ scripts/text_to_paragraphs.py
@@ -4,10 +4,12 @@
 import sys, os
 
 
-# Read the text file
-
 filename, extension = os.path.splitext (sys.argv[1])
 article_name = filename.split ("/") [-1]
+paragraph_path = "./scripts/paragraphs/" + article_name
+
+
+# Read the text file
 
 with open ( filename + extension, "r") as textfile:
     text = textfile.read ()
@@ -21,7 +23,7 @@ paragraphs[-1] = paragraphs[-1].strip()
 # Put each paragraph in a file inside the article folder (must be precreated)
 
 for i in range (len(paragraphs)):
-    p_filename = filename + "/" + article_name + "_" + str (i+1) + ".txt"
+    p_filename = paragraph_path + "/" + article_name + "_" + str (i+1) + ".txt"
 
     p_file = open (p_filename, 'w')
     p_file.write (paragraphs[i])

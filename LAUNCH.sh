10ec02ff03c74e0b1e00e6f60a6b4bf6986ac607
Hamza Ezzarouali El Boudri <hapaa@hotmail.fr>
2018-07-25 14:25:49 +0900

    Organizations in the repo

--- LAUNCH.sh
+++ LAUNCH.sh
@@ -3,11 +3,11 @@
 # Change these to make them work everywhere
 WORK_DIR=$PWD
 UCCA_DIR=$WORK_DIR/UCCA_Parser
+SCRIPTS=$WORK_DIR/scripts
 
 file=$1
 filename="${file##*/}"
 article_name="${filename%.*}"
-
 
 echo "+--------------------- Project Launch ----------------------+"
 echo "|      © Text Simplification/Internship Work - 2018         |"
@@ -34,23 +34,27 @@ else
     mkdir $UCCA_DIR/passages/$article_name/
 fi
 
-# # Split text to paragraphs                                                                          
-python $WORK_DIR/scripts/text_to_paragraphs.py $1
+sentence_dir=$UCCA_DIR/sentences/$article_name
+passage_dir=$UCCA_DIR/passages/$article_name
+
+# Split text to paragraphs                                                                          
+python $SCRIPTS/text_to_paragraphs.py $1
 echo "---------------------- Text split to paragraphs"
 
-# # Parse the paragraphs                                                                              
-python -m tupa $UCCA_DIR/sentences/$article_name/*.txt -m $WORK_DIR/models/ucca-bilstm
-mv *.xml $UCCA_DIR/passages/$article_name/
+# Parse the paragraphs                                                                              
+python -m tupa $sentence_dir/*.txt -m $WORK_DIR/models/ucca-bilstm
+mv *.xml $passage_dir
 echo "---------------------- Parsed the paragraphs"
 
 
 XML_DIR=$UCCA_DIR/passages/$article_name
 
+
 # util now the first argument should be an XML file to split
 # copy the spliting sentence into a text file
-python $WORK_DIR/_internship_work/_1_Split_sentence.py $XML_DIR > $WORK_DIR/_internship_work/Splitting.txt
-cat $WORK_DIR/_internship_work/Splitting.txt > $WORK_DIR/NeuralTextSimplification/data/test.en
-cat $WORK_DIR/_internship_work/Splitting.txt > $WORK_DIR/NeuralTextSimplification/data/test.sen
+python $SCRIPTS/_1_Split_sentence.py $XML_DIR > $sentence_dir/Splitting.txt
+cat $sentence_dir/Splitting.txt > $WORK_DIR/NeuralTextSimplification/data/test.en
+cat $sentence_dir/Splitting.txt > $WORK_DIR/NeuralTextSimplification/data/test.sen
 echo "---------------------- Splitting done"
 
 # Pass sentences through the Neural Component
@@ -62,8 +66,5 @@ echo "---------------------- Gone through the Neural Network"
 value=$(<result_NTS_epoch11_10.19.t7_5)
 echo "$value"
 
-
-echo "" > results_NTS # clear results file
-
 cd $WORK_DIR
-rm $XML_DIR/* # remove all XML files
+#rm $XML_DIR/* # remove all XML files

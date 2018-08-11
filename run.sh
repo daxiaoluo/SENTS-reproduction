e3b6e11d8f72c23cfc4aa09aab2732cba08e9ea1
Nabil <nabilzaim6991@gmail.com>
2018-08-10 22:23:48 -0700

    Web-development

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "+--------------------- Project Launch ----------------------+"
+echo "|      © Text Simplification/Internship Work - 2018         |"
+echo "+--------------------------------------------------------- -+"
+
+cd ..
+# $1 is the full path of the file, so pay attention on your makefile while passing the path
+make file=$1

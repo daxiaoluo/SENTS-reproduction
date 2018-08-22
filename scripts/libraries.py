48d89fd2159cf4e15b83e1a775ae18936f3b1aaf
nzaim001 <nabilzaim6991@gmail.com>
2018-08-21 20:19:34 -0700

    Create libraries.py

---
+++ scripts/libraries.py
@@ -0,0 +1,12 @@
+#####################################################################
+#                       Libraries:                                  #
+#####################################################################
+#!/usr/bin/python
+import numpy as np
+import matplotlib as plt
+# dealing with operations systems like reading a file
+import sys, os
+import xml.etree.ElementTree as ET
+from os import listdir
+from os.path import isfile, join
+import glob,re
